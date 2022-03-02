9d7abd40608ad2a9c13071e8ec342ebbdcdae852
Alan Pennell-Smith <al@render.com>
2022-03-02 12:14:16 +0000

    PR test

--- bin/init_deploy.sh
+++ bin/init_deploy.sh
@@ -7,19 +7,7 @@ SRV=$(echo $RENDER_INSTANCE_ID | cut -d '-' -f 1,2)
 
 if [[ "$IS_PULL_REQUEST" == "true" ]]; then
     echo "IS PR"
-
-    JSON='{"name":"%s"}\n'
-    DOMAIN_JSON=$(printf "$JSON" "$RENDER_SERVICE_NAME.heyhey.co.uk")
-    echo $DOMAIN_JSON
-
-    curl --request POST \
-        --url https://api.render.com/v1/services/$SRV/custom-domains \
-        --header "Accept: application/json" \
-        --header "Authorization: Bearer $RENKEY" \
-        --header "Content-Type: application/json" \
-        --data "$DOMAIN_JSON"
-
-    echo "Now create $RENDER_SERVICE_NAME.heyhey.co.uk CNAME record pointing at $RENDER_SERVICE_NAME.onrender.com..."
+    echo "This was a PR blueprint deploy! 🎉"
 else
     echo "This was a non PR main blueprint deploy! 🎉"
 fi
